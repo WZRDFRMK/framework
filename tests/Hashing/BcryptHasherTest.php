9e50745bd6a4298d843d0453ad0481ce778ff564
Kévin DUBUC <kevindubuc62@gmail.com>
2015-03-11 10:23:19 +0100

    test Hasher

---
+++ tests/Hashing/BcryptHasherTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Wzrd\Framework\Test\Hashing;
+
+use Mockery;
+use PHPUnit_Framework_TestCase;
+use Wzrd\Framework as Framework;
+
+class BcryptHasherTest extends PHPUnit_Framework_TestCase
+{
+    public function test_hash_and_veriify()
+    {
+        // Init ..
+        $hasher = new Framework\Hashing\BcryptHasher();
+
+        // Hash
+        $plain_value = 'value';
+        $hash = $hasher->hash($plain_value, array('cost' => 10));
+        $hash_with_different_cost = $hasher->hash($plain_value, array('cost' => 5));
+
+        // Test
+        $this->assertNotEquals($plain_value, $hash);
+        $this->assertNotEquals($hash, $hash_with_different_cost);
+        $this->assertTrue($hasher->verify($plain_value, $hash));
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        Mockery::close();
+    }
+}
