66cbdd3949b0321fb0bfc38746bfb62f2619b829
Kévin DUBUC <kevindubuc62@gmail.com>
2015-03-13 10:57:53 +0100

    add pushers handler

---
+++ src/Push/Handler.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Wzrd\Framework\Push;
+
+use Wzrd\Contracts\Push\Pusher;
+use Wzrd\Contracts\Push\Notification as NotificationContract;
+
+class Handler implements Pusher
+{
+    /**
+     * Pushers.
+     *
+     * @var Wzrd\Contracts\Push\Pusher[]
+     */
+    protected $pushers;
+
+    /**
+     * Construct with pushers instances.
+     *
+     * @param Wzrd\Contracts\Push\Pusher[] $pushers
+     */
+    public function __construct(array $pushers)
+    {
+        $this->pushers = $pushers;
+    }
+
+    /**
+     * Push message.
+     *
+     * @param  Wzrd\Contracts\Push\Notification  $notification
+     * @param  array  $options
+     */
+    public function push(NotificationContract $notification, array $options = array())
+    {
+        foreach($this->pushers as $pusher) {
+            if($pusher instanceof Pusher) {
+                $pusher->push($notification, $options);
+            }
+        }
+    }
+
+    /**
+     * Get supported platforms
+     *
+     * @return  array
+     */
+    public function getSupportedPlatforms()
+    {
+        $platforms = array();
+
+        foreach($this->pushers as $pusher) {
+            if($pusher instanceof Pusher) {
+                $platforms = array_merge($platforms, $pusher->getSupportedPlatforms());
+            }
+        }
+
+        return array_unique($platforms);
+    }
+}
---
+++ tests/Push/HandlerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Wzrd\Framework\Test\Push;
+
+use Mockery;
+use PHPUnit_Framework_TestCase;
+use Wzrd\Framework as Framework;
+
+class HandlerTest extends PHPUnit_Framework_TestCase
+{
+    public function test_send()
+    {
+        // Setup mailers
+        $pusher1 = $pusher2 = Mockery::mock('Wzrd\Contracts\Push\Pusher')->makePartial();
+        $pushers = array($pusher1, $pusher2, new \StdClass);
+        $pusher1->shouldReceive('getSupportedPlatforms')->andReturn(['ios', 'blackberry']);
+        $pusher2->shouldReceive('getSupportedPlatforms')->andReturn(['ios']);
+
+        // Configure handler
+        $handler = new Framework\Push\Handler($pushers);
+
+        // Prepare message
+        $notification = new Framework\Push\Notification();
+        $notification->setMessage('3-0 pour le RCL !');
+        $notification->setData(array('id_article' => 1568));
+        $notification->addDevices('ios', ['token1', 'token2'], array('sound' => 'goal.aif'));
+        $notification->addDevices('gcm', ['token3', 'token4'], array('title' => 'But !'));
+
+        // Prepare test
+        $pusher1->shouldReceive('push')->with($notification, array())->once();
+        $pusher2->shouldReceive('push')->with($notification, array())->once();
+
+        // Send !
+        $handler->push($notification);
+
+        // test platforms supported
+        $this->assertEquals(['ios', 'blackberry'], $handler->getSupportedPlatforms());
+    }
+}
