ef1415893b6b00e37009f719d69f2c6b3af06046
Kévin DUBUC <kevindubuc62@gmail.com>
2015-03-13 10:25:34 +0100

    add mailers handler

---
+++ src/Mail/Handler.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Wzrd\Framework\Mail;
+
+use Wzrd\Contracts\Mail\Mailer;
+use Wzrd\Contracts\Mail\Message as MessageContract;
+
+class Handler implements Mailer
+{
+    /**
+     * Mailers.
+     *
+     * @var Wzrd\Contracts\Mail\Mailer[]
+     */
+    protected $mailers;
+
+    /**
+     * Construct with mailers instances.
+     *
+     * @param Wzrd\Contracts\Mail\Mailer[] $mailers
+     */
+    public function __construct(array $mailers)
+    {
+        $this->mailers = $mailers;
+    }
+
+    /**
+	 * Send a mail
+	 *
+	 * @param  Wzrd\Contracts\Mail\Message  $message
+	 * @param  array  $options
+	 */
+	public function send(MessageContract $message, $options = array())
+    {
+        foreach($this->mailers as $mailer) {
+            if($mailer instanceof Mailer) {
+                $mailer->send($message, $options);
+            }
+        }
+    }
+}
---
+++ tests/Mail/HandlerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Wzrd\Framework\Test\Mail;
+
+use Mockery;
+use PHPUnit_Framework_TestCase;
+use Wzrd\Framework as Framework;
+
+class HandlerTest extends PHPUnit_Framework_TestCase
+{
+    public function test_send()
+    {
+        // Setup mailers
+        $mailer1 = $mailer2 = Mockery::mock('Wzrd\Contracts\Mail\Mailer')->makePartial();
+        $mailers = array($mailer1, $mailer2, new \StdClass);
+
+        // Configure handler
+        $handler = new Framework\Mail\Handler($mailers);
+
+        // Prepare message
+        $message = new Framework\Mail\Message();
+        $message->addFrom('from@sdis62.fr');
+        $message->addFrom('from@domain.fr', 'From');
+        $message->addTo('to@sdis62.fr');
+        $message->addTo('to@domain.fr', 'To');
+        $message->addCc('cc@sdis62.fr');
+        $message->addCc('cc@domain.fr', 'Cc');
+        $message->addBcc('bcc@sdis62.fr');
+        $message->addBcc('bcc@domain.fr', 'Bcc');
+        $message->setSubject('Subject');
+        $message->setText('Text');
+        $message->setHtml('<p>HTML</p>');
+        $message->attach('filename.jpg', array('filename' => 'cool.jpg', 'content-type' => 'image/jpg'));
+        $message->inline('filename2.jpg', array('filename' => 'yeah.jpg'));
+
+        // Prepare test
+        $mailer1->shouldReceive('send')->with($message, array())->once();
+        $mailer2->shouldReceive('send')->with($message, array())->once();
+
+        // Send !
+        $handler->send($message);
+    }
+}
