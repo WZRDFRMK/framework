2d1a35d9df1b3871a1041f0d939cec5d780852a6
Kévin DUBUC <kevindubuc62@gmail.com>
2015-05-06 11:43:55 +0200

    improve collections in fractal transformer

--- src/Transformer/FractalTransformer.php
+++ src/Transformer/FractalTransformer.php
@@ -3,7 +3,11 @@
 namespace WZRD\Transformer;
 
 use League\Fractal;
+use Pagerfanta\Pagerfanta;
+use Pagerfanta\Adapter\ArrayAdapter;
+use League\Fractal\TransformerAbstract;
 use WZRD\Contracts\Transformer\Transformer;
+use League\Fractal\Pagination\PagerfantaPaginatorAdapter;
 
 class FractalTransformer implements Transformer
 {
@@ -14,21 +18,30 @@ class FractalTransformer implements Transformer
       */
      protected $fractal;
 
+     /**
+      * Function to generate a route (for collections).
+      *
+      * @var callable
+      */
+     protected $route_generator;
+
     /**
      * Create a new fractal transformer instance.
      *
      * @param League\Fractal\Manager $fractal
+     * @param callable $route_generator
      */
-    public function __construct(Fractal\Manager $fractal)
+    public function __construct(Fractal\Manager $fractal, callable $route_generator = null)
     {
         $this->fractal = $fractal;
+        $this->route_generator = is_callable($route_generator) ? $route_generator : function($page) {return $page;};
     }
 
     /**
      * Transform a response with a transformer.
      *
      * @param mixed  $value
-     * @param object $transformer
+     * @param League\Fractal\TransformerAbstract $transformer
      * @param array  $includes    Optional
      *
      * @return array
@@ -39,17 +52,34 @@ class FractalTransformer implements Transformer
             $this->fractal->parseIncludes(implode(',', $includes));
         }
 
-        if (is_array($value)) {
-            // TODO : support cursor current / prev / next
+        $resource = $this->createResource($value, $transformer);
+
+        return $this->fractal->createData($resource)->toArray();
+    }
+
+    /**
+     * Create a Fractal resource instance.
+     *
+     * @param mixed  $value
+     * @param League\Fractal\TransformerAbstract $transformer
+     *
+     * @return League\Fractal\Resource\ResourceAbstract
+     */
+    protected function createResource($value, TransformerAbstract $transformer)
+    {
+        if($value instanceof Pagerfanta) {
+            $resource = new Fractal\Resource\Collection($value, $transformer);
+            $resource->setPaginator(new PagerfantaPaginatorAdapter($value, $this->route_generator));
+        }
+        else if (is_array($value)) {
             $cursor = new Fractal\Pagination\Cursor(null, null, null, count($value));
             $resource = new Fractal\Resource\Collection($value, $transformer);
             $resource->setCursor($cursor);
-        } else {
+        }
+        else {
             $resource = new Fractal\Resource\Item($value, $transformer);
         }
 
-        $data = $this->fractal->createData($resource);
-
-        return $data->toArray();
+        return $resource;
     }
 }
--- tests/Transformer/FractalTransformerTest.php
+++ tests/Transformer/FractalTransformerTest.php
@@ -6,37 +6,56 @@ use Mockery;
 use League\Fractal;
 use PHPUnit_Framework_TestCase;
 use WZRD as Framework;
+use Pagerfanta\Adapter\ArrayAdapter;
+use Pagerfanta\Pagerfanta;
 
 class FractalTransformerTest extends PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
         $this->fractal = Mockery::mock('League\Fractal\Manager')->makePartial();
-        $this->transformer = new Framework\Transformer\FractalTransformer($this->fractal);
+        $this->transformer = new Framework\Transformer\FractalTransformer($this->fractal, function($page) {return $page;});
     }
 
     public function test_transform_single_value()
     {
         // Prepare ..
+        $transformer = Mockery::mock('League\Fractal\TransformerAbstract');
         $scope = Mockery::mock('League\Fractal\Scope')->makePartial();
         $scope->shouldReceive('toArray')->andReturn(true)->once();
         $this->fractal->shouldReceive('parseIncludes')->with('include1,include2')->once();
         $this->fractal->shouldReceive('createData')->with(Mockery::type('League\Fractal\Resource\Item'))->andReturn($scope)->once();
 
         // Transform ..
-        $this->assertTrue($this->transformer->transform('user', 'UserTransformer', array('include1', 'include2')));
+        $this->assertTrue($this->transformer->transform('user', $transformer, array('include1', 'include2')));
     }
 
-    public function test_transform_collection()
+    public function test_transform_collection_array()
     {
         // Prepare ..
+        $transformer = Mockery::mock('League\Fractal\TransformerAbstract');
         $scope = Mockery::mock('League\Fractal\Scope')->makePartial();
         $scope->shouldReceive('toArray')->andReturn(true)->once();
         $this->fractal->shouldReceive('parseIncludes')->with('include1,include2')->once();
         $this->fractal->shouldReceive('createData')->with(Mockery::type('League\Fractal\Resource\Collection'))->andReturn($scope)->once();
 
         // Transform ..
-        $this->assertTrue($this->transformer->transform(array('user', 'user'), 'UserTransformer', array('include1', 'include2')));
+        $this->assertTrue($this->transformer->transform(array('user', 'user'), $transformer, array('include1', 'include2')));
+    }
+
+    public function test_transform_collection_pagerfanta()
+    {
+        // Prepare ..
+        $transformer = Mockery::mock('League\Fractal\TransformerAbstract');
+        $adapter = new ArrayAdapter(array('user', 'user'));
+        $pagerfanta = new Pagerfanta($adapter);
+        $scope = Mockery::mock('League\Fractal\Scope')->makePartial();
+        $scope->shouldReceive('toArray')->andReturn(true)->once();
+        $this->fractal->shouldReceive('parseIncludes')->with('include1,include2')->once();
+        $this->fractal->shouldReceive('createData')->with(Mockery::type('League\Fractal\Resource\Collection'))->andReturn($scope)->once();
+
+        // Transform ..
+        $this->assertTrue($this->transformer->transform($pagerfanta, $transformer, array('include1', 'include2')));
     }
 
     public function tearDown()
